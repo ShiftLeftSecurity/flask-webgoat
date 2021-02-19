c32880dbbfaed396a3262daa593d970cf6c37267
Claudiu-Vlad Ursache <claudiu@cvursache.com>
2021-02-19 15:18:19 +0100

    Add initial implementation (#1)

---
+++ flask_webgoat/status.py
@@ -0,0 +1,14 @@
+from flask import (
+    Blueprint, jsonify
+)
+
+bp = Blueprint('status', __name__)
+
+@bp.route('/status')
+def status():
+    return jsonify({'success': True})
+
+
+@bp.route('/ping')
+def ping():
+    return jsonify({'success': True})
